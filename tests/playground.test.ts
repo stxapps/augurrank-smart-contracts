eea448f979da9708cf147a628f2353783955831e
wit <1229910+0xc22b@users.noreply.github.com>
2024-11-29 11:42:33 +0700

    Add tests and deployment plan for playground.clar

--- tests/playground.test.ts
+++ tests/playground.test.ts
@@ -1,21 +1,24 @@
-
-import { describe, expect, it } from "vitest";
+import { expect, it } from "vitest";
+import { Cl } from '@stacks/transactions';
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const wallet1 = accounts.get("wallet_1")!;
 
 /*
   The test below is an example. To learn more, read the testing documentation here:
   https://docs.hiro.so/stacks/clarinet-js-sdk
 */
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
+it("ensures simnet is well initalised", () => {
+  expect(simnet.blockHeight).toBeDefined();
+});
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+it("get-heights and get-usd-per-btc", () => {
+  let cRes;
+
+  cRes = simnet.callReadOnlyFn('playground', 'get-heights', [Cl.uint(1)], wallet1);
+  console.log(JSON.stringify(cRes.result));
+
+  cRes = simnet.callReadOnlyFn('playground', 'get-usd-per-btc', [Cl.uint(1)], wallet1);
+  console.log(JSON.stringify(cRes.result));
 });
